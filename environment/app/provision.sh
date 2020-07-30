218ed5a6a8597abf8a5e22fb12a87f54841fec48
marwai <mtse@spartaglobal.com>
2020-07-30 19:08:45 +0100

    reverse nginx proxy

---
+++ environment/app/provision.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo su
+sudo apt-get install python-software-properties
+sudo su
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo su
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install
+
+# setting up the app
+export DB_HOST="mongodb://192.168.10.150:27017/posts"
+cd /home/ubuntu/app
+sudo su
+npm install
+node app.js
