74f02346c5f7144dde57ac9b122b9c9cd9d50da2
marwai <mtse@spartaglobal.com>
2020-07-31 15:34:31 +0100

    updating app/ provisions folder

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -2,10 +2,17 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
 echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-sudo apt-get clean -y
-sudo rm -rf /var/lib/apt/lists/*
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get -clean
+sudo apt-get update
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt-get update
+sudo rm -rf /var/lib/apt/lists/partial
+sudo apt-get update -o Acquire::CompressionTypes::Order::=gz
+sudo apt-get upgrade -y
+sudo apt-get update -y
+
 
 # sudo apt-get install mongodb-org=3.2.20 -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
